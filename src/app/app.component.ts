b13dee9b3e1dfd6818def003eb372a13f0d04f03
rosawiyananda <rosa1800016106@webmail.uad.ac.id>
2020-03-29 06:17:14 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './HalamanUtama.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tekweb1800016106';
+}
